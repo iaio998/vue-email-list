51fee279d25f0ee89582ee445e8149a6c221c54e
iaio998 <depalatisiacopo@gmail.com>
2023-11-07 16:14:56 +0100

    Exercise complete

--- js/script.js
+++ js/script.js
@@ -3,25 +3,18 @@ const { createApp } = Vue;
 const miaApp = createApp({
   data() {
     return {
-      numbers: [],
-      word: "",
+      mails: [],
     };
   },
   methods: {},
   mounted() {
     for (let i = 0; i < 10; i++) {
       axios
-        .get("https://flynn.boolean.careers/exercises/api/random/int")
-        .then((resp) => {
-          console.log(resp.data.response);
-          this.numbers.push(resp.data.response);
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((el) => {
+          console.log(el.data.response);
+          this.mails.push(el.data.response);
         });
     }
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/word")
-      .then((resp) => {
-        console.log(resp.data.response);
-        this.word = resp.data.response;
-      });
   },
 }).mount("#app");
